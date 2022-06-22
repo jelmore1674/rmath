47d05ba8c834513b6cdb6bb19db1cb791d1cfb07
Justin Elmore <jelmore@gsandf.com>
2022-06-22 15:19:11 -0500

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::env::args;
+
+
+fn main() {
+    let first_argument: String = args().nth(1).unwrap();
+    let second_argument: String = args().nth(2).unwrap();
+    let third_argument: String = args().nth(3).unwrap();
+    
+    match second_argument.as_str() {
+        "plus" =>  {
+            let math: i32 = first_argument.parse::<i32>().unwrap() + third_argument.parse::<i32>().unwrap();
+            println!("{}", math);
+        }
+         "minus" =>  {
+            let math: i32 = first_argument.parse::<i32>().unwrap() - third_argument.parse::<i32>().unwrap();
+            println!("{}", math);
+        },
+         "divide" =>  {
+            let math: i32 = first_argument.parse::<i32>().unwrap() / third_argument.parse::<i32>().unwrap();
+            println!("{}", math);
+        },
+         "multiply" | "times" =>  {
+            let math: i32 = first_argument.parse::<i32>().unwrap() * third_argument.parse::<i32>().unwrap();
+            println!("{}", math);
+        }
+        _ => println!("Not a valid argument"),
+    }
+}
