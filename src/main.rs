b7b4356487f22866c9302c64001d58dadcd09419
Justin Elmore <jelmore1674@pm.me>
2022-06-22 23:12:21 -0500

    move math to own directory, clean up code, and added percentage functionality

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,23 @@
 use std::env::args;
+mod math;
+use math::math::{*};
+use math::math::Args;
+
 
 
 fn main() {
     let first_argument: String = args().nth(1).unwrap();
     let second_argument: String = args().nth(2).unwrap();
     let third_argument: String = args().nth(3).unwrap();
+
+    let percentage: Vec<&str> = first_argument.split("%").collect();
+
+    let args = Args { 
+        first_arg: percentage[0].to_string(), 
+        second_arg:second_argument, 
+        third_arg: third_argument
+    };
+
+    math(args)
     
-    match second_argument.as_str() {
-        "plus" | "+" =>  {
-            let math: i32 = first_argument.parse::<i32>().unwrap() + third_argument.parse::<i32>().unwrap();
-            println!("{}", math);
-        }
-         "minus" | "-" =>  {
-            let math: i32 = first_argument.parse::<i32>().unwrap() - third_argument.parse::<i32>().unwrap();
-            println!("{}", math);
-        },
-         "divide" | "/" =>  {
-            let math: i32 = first_argument.parse::<i32>().unwrap() / third_argument.parse::<i32>().unwrap();
-            println!("{}", math);
-        },
-         "multiply" | "times" | "*" =>  {
-            let math: i32 = first_argument.parse::<i32>().unwrap() * third_argument.parse::<i32>().unwrap();
-            println!("{}", math);
-        }
-        _ => println!("Not a valid argument"),
-    }
 }
---
+++ src/math/math.rs
@@ -0,0 +1,54 @@
+pub struct Args {
+    pub first_arg: String,
+    pub second_arg: String,
+    pub third_arg: String
+}
+
+/// Take the args if a standard math problem is presented. Otherwise it will not run
+/// The args will be passed through. If the arg is of it will get the percentage
+pub fn math(args: Args) {
+    match args.second_arg.as_str() {
+        "plus" =>  {
+            let math = add(args.first_arg.parse::<f64>().unwrap(),args.third_arg.parse::<f64>().unwrap());
+            println!("{}", math);
+        }
+         "minus"  =>  {
+            let math = subtract(args.first_arg.parse::<f64>().unwrap(),args.third_arg.parse::<f64>().unwrap());
+            println!("{}", math);
+        },
+         "divide"  =>  {
+            let math = divide(args.first_arg.parse::<f64>().unwrap(),args.third_arg.parse::<f64>().unwrap());
+            println!("{}", math);
+        },
+         "times"  =>  {
+            let math = multiply(args.first_arg.parse::<f64>().unwrap(),args.third_arg.parse::<f64>().unwrap());
+            println!("{}", math);
+        },
+        "of" => {
+            let math = get_percentage(args.first_arg.parse::<f64>().unwrap(),args.third_arg.parse::<f64>().unwrap());
+            println!("{}", math);
+        }
+        _ => println!("Not a valid argument"),
+    }
+}
+
+fn add(x:f64,y:f64) -> f64 {
+   return x + y;
+}
+
+fn subtract(x:f64,y:f64) -> f64 {
+   return x - y;
+}
+
+fn divide(x:f64,y:f64) -> f64 {
+   return x / y;
+}
+
+fn multiply(x:f64,y:f64) -> f64 {
+   return x * y;
+}
+
+fn get_percentage(x:f64,y:f64) -> f64 {
+    let percent = x / 100.0;
+    return percent * y;
+}
